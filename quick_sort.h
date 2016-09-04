d26c2ec5c1172c5ce604755284bfebe5103c0031
wfxr <wfxrgm@gmail.com>
2016-09-04 08:35:07 +0800

    refactor

--- quick_sort.h
+++ quick_sort.h
@@ -5,10 +5,10 @@ void quick_sort1(vector<int> &a, int l, int u) {
     auto t = a[l];
     auto i = l + 1, j = u;
     while (i <= j)
-        if (t < a[i])
-            swap(a[i], a[j--]);
-        else
+        if (a[i] < t)
             ++i;
+        else
+            swap(a[i], a[j--]);
 
     swap(a[l], a[j]);
     quick_sort1(a, l, j - 1);
@@ -20,9 +20,13 @@ void quick_sort2(vector<int> &a, int l, int u) {
     auto t = a[l];
     auto i = l, j = u + 1;
     for (;;) {
-        do ++i; while (i <= u && a[i] < t);
+        do
+            ++i;
+        while (i <= u && a[i] < t);
         // 因为j向左移动时必然经过t，此时a[j] > t不成立，循环结束
-        do --j; while (a[j] > t);
+        do
+            --j;
+        while (a[j] > t);
         if (i > j) break;
         // i指向的是（从前往后）第一个大于t的元素
         // j指向的是（从后往前）第一个小于t的元素
